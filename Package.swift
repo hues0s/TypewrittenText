0c0f95b6789c0e595dda06ef66bd1e8238f45479
Héctor Ullate <hector.ullate@gmail.com>
2022-12-29 18:46:25 +0100

    Cleanup

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .target(
             name: "TypewrittenText",
             dependencies: [],
-            exclude: ["demo.gif", "logo.png"]),
+            exclude: ["Images/demo.gif", "Images/logo.png"]),
     ]
 )
